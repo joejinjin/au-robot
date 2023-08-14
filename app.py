9818c5cc880cbfd4590d8654e3f6da889848f6ac
zhuangjiaxin <zhuangjiaxin@52tt.com>
2023-08-14 23:05:59 +0800

    Test Token

--- app.py
+++ app.py
@@ -57,6 +57,7 @@ def running_question(user: str, content: str):
     user_cache[user] = "0"
     messages = [{"role": "user", "content": content}]
     result = chat_completion(messages)
+    print("result => %s" % result)
     user_cache[user] = result
 
 
