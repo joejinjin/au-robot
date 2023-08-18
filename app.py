c2153b71a28d775c1df2f9db13ffd17f87b2ab54
zhuangjiaxin <zhuangjiaxin@52tt.com>
2023-08-18 19:25:48 +0800

    换行问题

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import uvicorn
 import hashlib
-from fastapi import FastAPI, Request, BackgroundTasks
+from fastapi import FastAPI, Request, BackgroundTasks, Response
 from fastapi.responses import PlainTextResponse
-from fastapi.responses import XMLResponse
 
 import reply
 import receive
@@ -29,7 +28,7 @@ def token(request: Request):
 
 @webapp.post("/token")
 async def access(request: Request, background: BackgroundTasks):
-    headers = {"Content-Type": "application/xml"}
+    media_type = "application/xml"
 
     data = (await request.body()).decode("utf-8")
     print("user => %s" % data)
@@ -42,15 +41,15 @@ async def access(request: Request, background: BackgroundTasks):
         if msg.FromUserName not in user_cache:
             background.add_task(running_question, msg.FromUserName, msg.Content.decode("utf-8"))
             text_msg = reply.TextMsg(to_user, from_user, "5秒后提醒我回复～")
-            return XMLResponse(content=text_msg.send(), headers=headers)
+            return Response(content=text_msg.send(), media_type=media_type)
 
         if user_cache[msg.FromUserName] == "0":
             text_msg = reply.TextMsg(to_user, from_user, "再等等不着急～")
-            return XMLResponse(content=text_msg.send(), headers=headers)
+            return Response(content=text_msg.send(), media_type=media_type)
 
         result = user_cache.pop(msg.FromUserName)
         text_msg = reply.TextMsg(to_user, from_user, result)
-        return XMLResponse(content=text_msg.send(), headers=headers)
+        return Response(content=text_msg.send(), media_type=media_type)
 
     return "success"
 
